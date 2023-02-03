d9bc2db8c5c8b0025dd4934277502777af2f24de
Benedict McElroy <codingwithbenedict99@gmail.com>
2023-02-03 15:59:58 +0000

    api is working

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=f2e440a79117f6995bf30b0b003b5680'
-var queryURL = 'https://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={f2e440a79117f6995bf30b0b003b5680}'
-
-console.log('Hello World');
+// 
 
 
 var button = $('<button>');
@@ -9,14 +6,52 @@ $(button).addClass('btn btn-styled')
 $(button).text('LONDON');
 $(button).appendTo('.list-group');
 
+
 $('.form').on('submit', function(event) {
     event.preventDefault();
+    
     alert('hey');
+    
 })
 
 $('#search-button').on('click', function(event) {
 event.preventDefault();
 var formInput = $('.form-input').val();
+var cityName = formInput;
+var latLong = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=f2e440a79117f6995bf30b0b003b5680'
+console.log(latLong);
+
+$.ajax({
+url: latLong,
+method: "GET"
+}).then(function(response) {
+console.log(response);
+
+console.log(response[0].lat)
+
+localStorage.setItem('latitude', response[0].lat);
+localStorage.setItem('longitude', response[0].lon);
+
+
+var currentLatitude = localStorage.getItem('latitude');
+var currentLongitude = localStorage.getItem('longitude');
+
+var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + currentLatitude + '&lon=' + currentLongitude + '&appid=f2e440a79117f6995bf30b0b003b5680&units=metric'
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then(function(response) {
+    console.log(response);
+    console.log(`temp: ${response.list[0].main.temp},   
+    humidity: ${response.list[0].main.humidity}, 
+    wind: ${response.list[0].wind.speed}`)
+    })
+
+})
+
+
+
 var buttonText = formInput.trim().toUpperCase();
 console.log(buttonText);
 var newBtn = $('<button>');
@@ -24,5 +59,7 @@ newBtn.appendTo('.list-group');
 newBtn.text(buttonText);
 newBtn.addClass('btn-styled');
 $('.form-input').val('');
+
+
 })
 
